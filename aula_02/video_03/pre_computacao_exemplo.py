0cb0ea9f36998b39524f3956c52f0db1ad0d3935
Erick Muller <erick@em.pro.br>
2025-06-26 12:55:52 -0300

    melhorando exemplo aula 02 video 03

--- aula_02/video_03/pre_computacao_exemplo.py
+++ aula_02/video_03/pre_computacao_exemplo.py
@@ -1,23 +1,64 @@
+import random
+import timeit
+
 DESCONTOS = {"0": 0, "10": 0.05, "20": 0.10, "50": 0.15}
+DESCONTOS_KEYS = tuple(reversed(sorted(DESCONTOS.keys())))
+
+precos = [random.randint(10, 100) for _ in range(200_000)]
+quantidades = [x for x in range(1, 61)]
 
 
 def calcular_desconto(quantidade):
-    d = sorted(DESCONTOS.keys())
-    for limite in reversed(d):
+    for limite in reversed(DESCONTOS_KEYS):
         if quantidade >= int(limite):
             return DESCONTOS[limite]
 
 
+t1 = timeit.default_timer()
+for i in range(len(precos)):
+    q = random.choice(quantidades)
+    [precos[i], q, calcular_desconto(q)]
+t2 = timeit.default_timer()
+print(t2 - t1)
+
 # -----------
+
+print("-" * 80)
+
 
 CACHE_DESCONTOS = {}
 
 
 def calcular_desconto_cache(quantidade):
-    if quantidade in CACHE_DESCONTOS:
+    if CACHE_DESCONTOS and quantidade in CACHE_DESCONTOS:
         return CACHE_DESCONTOS[quantidade]
     else:
-        d = sorted(DESCONTOS.keys())
-        for limite in reversed(d):
+        for limite in reversed(DESCONTOS_KEYS):
             if quantidade >= int(limite):
+                CACHE_DESCONTOS[quantidade] = DESCONTOS[limite]
                 return DESCONTOS[limite]
+
+
+t1 = timeit.default_timer()
+for i in range(len(precos)):
+    q = random.choice(quantidades)
+    (precos[i], q, calcular_desconto_cache(q))
+t2 = timeit.default_timer()
+print(t2 - t1)
+
+print("-" * 80)
+
+
+# @functools.lru_cache(200)
+# def calcular_desconto_mru(quantidade):
+#     for limite in reversed(DESCONTOS_KEYS):
+#         if quantidade >= int(limite):
+#             return DESCONTOS[limite]
+
+
+# t1 = timeit.default_timer()
+# for i in range(len(precos)):
+#     q = random.choice(quantidades)
+#     [precos[i], q, calcular_desconto_mru(q)]
+# t2 = timeit.default_timer()
+# print(t2 - t1)
